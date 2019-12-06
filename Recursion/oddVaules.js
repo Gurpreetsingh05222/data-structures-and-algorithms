f983219e05c6102821c193b116b668407c0d9991
Gurpreet <gurpreetsingh05222@gmail.com>
2019-12-06 15:06:07 +0530

    without helper function

--- Recursion/oddVaules.js
+++ Recursion/oddVaules.js
@@ -1,21 +1,39 @@
 // Identify the odd values in a array?
-function oddValues(arr){ //helper function
-    let result = [];
+// function oddValues(arr){ //helper function
+//     let result = [];
 
-    function helper(input){
-        if(input.length === 0){
-            return;
-        }
+//     function helper(input){
+//         if(input.length === 0){
+//             return;
+//         }
 
-        if(input[0] % 2 !== 0){
-            result.push(input[0]);
-        }
+//         if(input[0] % 2 !== 0){
+//             result.push(input[0]);
+//         }
 
-        helper(input.slice(1));
+//         helper(input.slice(1));
+//     }
+
+//     helper(arr);
+//     return result;
+// }
+
+// console.log(oddValues([1,2,3,4,5,6,7,8,9]));
+
+// Without helper function
+function collectOdd(arr){
+    let newArr = [];
+
+    if(arr.length === 0){
+        return newArr;
     }
 
-    helper(arr);
-    return result;
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+
+    newArr = newArr.concat(collectOdd(arr.slice(1)));
+    return newArr;
 }
 
-console.log(oddValues([1,2,3,4,5,6,7,8,9]));
+console.log(collectOdd([1,2,3,4,5]));
