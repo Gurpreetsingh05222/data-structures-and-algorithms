5f360eefa8d6d8f9e18c9748d30cba6702b713f9
unknown <gurpreetsingh05222@gmail.com>
2019-12-10 22:23:07 +0530

    basic bubble sort implementation

---
+++ searching and sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr){
+    const swap = (arr1, indx1, indx2) =>{
+        [arr[indx1], arr[indx2]] = [arr[indx2], arr[indx1]];
+    }
+
+    for(let i=arr.length; i>0; i--){
+        for(let j=0; j<i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr,j,j+1);
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([23,35,29,8]));
