775cbd8f5bdb52ad24bfd0c9332236e518094bea
unknown <gurpreetsingh05222@gmail.com>
2019-12-14 22:15:52 +0530

    shift functionality singly linked list

--- Linked list/singly_linked_list.js
+++ Linked list/singly_linked_list.js
@@ -42,11 +42,16 @@ class SinglyLinkedList{
         }
         return current;
     }
+    shift(){
+        if(!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
 }
 
 let list = new SinglyLinkedList();
-list.push("Hi");
-list.push("there");
-console.log(list);
-list.pop();
-console.log(list);
