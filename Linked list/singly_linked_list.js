2d70b1f5b95406676c7a45ec3aada93ccb2270c1
unknown <gurpreetsingh05222@gmail.com>
2019-12-18 11:24:15 +0530

    remove singly linked list

--- Linked list/singly_linked_list.js
+++ Linked list/singly_linked_list.js
@@ -86,7 +86,7 @@ class SinglyLinkedList{
         if(index < 0 || index > this.length) return false;
         if(index === this.length) return !!this.push(val);
         if(index === 0) return this.unshift(val);
-        
+
         let newNode = new Node(val);
         let prev = this.get(index-1);
         let temp = prev.next;
@@ -94,6 +94,16 @@ class SinglyLinkedList{
         newNode.next = temp;
         this.length++;
         return true;
+    }
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        let previousNode = this.get(index - 1);
+        let removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
     }
 }
 
