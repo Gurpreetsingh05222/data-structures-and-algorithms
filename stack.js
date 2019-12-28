5964ac97c20ece994e4d755cf68e2930a994e504
unknown <gurpreetsingh05222@gmail.com>
2019-12-28 09:41:06 +0530

    stack and push method

---
+++ stack.js
@@ -0,0 +1,28 @@
+//Stack implementation
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class stack{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+}
