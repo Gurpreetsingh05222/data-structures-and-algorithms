0cb4babe39f2f6353edae4f574c0bea3a4e8704c
unknown <gurpreetsingh05222@gmail.com>
2020-02-28 16:17:06 +0530

    max binary heap insert

---
+++ Heaps/Max_binary_heap.js
@@ -0,0 +1,24 @@
+class MaxBinaryHeap{
+    constructor(){
+        this.values = [41,39,33,18,27,12];
+    }
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp;
+    }
+
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.value[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.value[parentIdx];
+            if(element <= parent)break;
+            this.value[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+}
+
+let heap = new MaxBinaryHeap();
