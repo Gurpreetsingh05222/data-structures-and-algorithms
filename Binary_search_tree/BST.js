f98b20d4ff4eabb24d0b4227648e9b57b38636cf
unknown <gurpreetsingh05222@gmail.com>
2020-02-28 08:55:31 +0530

    binary search tree

--- Binary_search_tree/BST.js
+++ Binary_search_tree/BST.js
@@ -11,6 +11,7 @@ class BinarySearchTree{
         this.root = null;
     }
 
+    //Inserting the values
     insert(value){
         let newNode = new Node(value);
         if(this.root === null){
@@ -35,6 +36,8 @@ class BinarySearchTree{
             }
         }
     }
+
+    //Finding the values
     find(value){
         if(this.root === null) return false;
         let current = this.root,
@@ -50,66 +53,9 @@ class BinarySearchTree{
         }
         return false;
     }
-
-    BFS(){
-        let node = this.root,
-            data = [],
-            queue = [];
-        queue.push(this.root);
-        while(queue.length){
-            node = queue.shift();
-            data.push(node.value);
-            if(node.left) queue.push(node.left);
-            if(node.right) queue.push(node.right);
-        }
-        return data;
-    }
-
-    DFSPreOrder(){
-        let data = [];
-        let current = this.root;
-        function traverse(node){
-            data.push(node.value);
-            if(node.left) traverse(node.left);
-            if(node.right) traverse(node.right);
-        }
-        traverse(current);
-        return data;
-    }
-
-    DFSPostOrder(){
-        let data = [];
-        let current = this.root;
-        function traverse(node){
-            if(node.left) traverse(node.left);
-            if(node.right) traverse(node.right);
-            data.push(node.value);
-        }
-        traverse(current);
-        return data;
-    }
-
-    DFSInOrder(){
-        let data = [];
-        function traverse(node){
-            node.left && traverse(node.left);
-            data.push(node.value);
-            node.right && traverse(node.right);
-        }
-        traverse(this.root);
-        return data;
-    }
 }
 
 let tree = new BinarySearchTree();
-tree.insert(10);
-tree.insert(6);
-tree.insert(15);
-tree.insert(3);
-tree.insert(8);
-tree.insert(20);
-
-// console.log(tree.BFS()); //10,6,15,3,8,20 (expected output)
-console.log(tree.DFSPreOrder()); //10,6,3,8,15,20 (expected output)
-console.log(tree.DFSPostOrder()); //3,8,6,20,15,10 (expected output)
-console.log(tree.DFSInOrder()); //3,8,6,20,15,10 (expected output)
+console.log(tree.insert(10));
+console.log(tree.insert(11));
+console.log(tree.insert(4));
