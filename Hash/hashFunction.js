700d74a2bfcf77910ba996b991aec1cb008a8525
unknown <gurpreetsingh05222@gmail.com>
2020-03-12 22:20:29 +0530

    improved hash function

--- Hash/hashFunction.js
+++ Hash/hashFunction.js
@@ -1,8 +1,10 @@
 function hash(key, arrayLen){
     let total = 0;
-    for(let char of key){
+    let prime = 31;
+    for(let i = 0; i < Math.min(key.length, 100); i++){
+        let char = key[i];
         let value = char.charCodeAt(0) - 96
-        total = (total + value) % arrayLen;
+        total = (total * prime + value) % arrayLen;
     }
     return total;
 }
