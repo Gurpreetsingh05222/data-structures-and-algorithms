2ad01d2dace82c48a6ecb1a077e6e66e38871274
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2020-09-11 07:58:33 -0400

    add edge graph

--- Graph/Graph_AddVertex.js
+++ Graph/Graph_AddVertex.js
@@ -6,4 +6,18 @@ class Graph{
     addVertex(vertex){
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
+    addEdge(v1, v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
 }
+
+let g = new Graph();
+g.addVertex("Montreal");
+g.addVertex("Delhi");
+g.addVertex("Toronto");
+
+g.addEdge("Montreal", "Toronto");
+
+console.log(g);
